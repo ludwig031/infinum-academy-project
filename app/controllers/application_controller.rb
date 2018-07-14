28e218ba642138becfb93d5bb4a08c4790ce2e7a
Borna Matijanic <borna.matijanic@gmail.com>
2018-07-14 23:33:42 +0200

    Add controller for /world-cup endpoint

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
+# frozen_string_literal: true
+
+# ApplicationController class inherits a constructor from ActionController::Base
 class ApplicationController < ActionController::Base
+  def matches
+    output = if request.query_parameters.present?
+               WorldCup.matches_on('2018-07-11')
+             else
+               WorldCup.matches
+             end
+    render json: output.as_json
+  end
 end
