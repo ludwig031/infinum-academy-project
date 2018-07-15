6116c3666dde5874598455060ff3e88aa7b72808
Borna Matijanic <borna.matijanic@gmail.com>
2018-07-15 20:54:23 +0200

    Implement event specs

---
+++ spec/services/world_cup/event_spec.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+RSpec.describe 'event class', :event do
+  attr_accessor :event
+
+  before(:all) do
+    @event = WorldCup::Event.new(
+      'id' => 402,
+      'type_of_event' => 'goal',
+      'player' => 'Luka MODRIC',
+      'time' => "80'"
+    )
+  end
+
+  describe 'Event#id', :event_id do
+    it 'returns event id' do
+      expect(event.id).to eq(402)
+    end
+  end
+
+  describe 'Event#type', :event_type do
+    it 'returns event type' do
+      expect(event.type).to eq('goal')
+    end
+  end
+
+  describe 'Event#player', :event_player do
+    it 'returns event player' do
+      expect(event.player).to eq('Luka MODRIC')
+    end
+  end
+
+  describe 'Event#time', :event_time do
+    it 'returns event id' do
+      expect(event.time).to eq("80'")
+    end
+  end
+
+  describe 'Event#to_s', :event_to_string do
+    it 'returns modified event string' do
+      expect(event.to_s).to eq("#402: goal@80' - Luka MODRIC")
+    end
+  end
+
+end
