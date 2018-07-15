7e01ca76355ab60b96443cb3b30fa2b6656c935a
Borna Matijanic <borna.matijanic@gmail.com>
2018-07-15 04:42:35 +0200

    Change time zone

--- app/services/world_cup/match.rb
+++ app/services/world_cup/match.rb
@@ -19,7 +19,7 @@ module WorldCup
 
     def starts_at
       starts_at = match_hash['datetime']
-      Time.zone = 'Moscow'.parse(starts_at).to_time
+      Time.zone.iso8601.parse(starts_at).to_time
     end
 
     def home_team_goals
