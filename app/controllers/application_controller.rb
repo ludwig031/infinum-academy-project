3ecfc81648893fd56891a23609f252a3dbdaeb29
Borna Matijanic <borna.matijanic@gmail.com>
2018-07-16 15:13:20 +0200

    Change date validation method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,12 @@
 # ApplicationController class inherits a constructor from ActionController::Base
 class ApplicationController < ActionController::Base
-  def validate_date(string)
-    string.match(/\d{4}-\d{2}-\d{2}/)
-  end
-
   def matches
-    date = params[:date]
-    output = if date && validate_date(date)
+    output = if params[:date]
+               date = Date.parse(params[:date]).strftime('%Y-%m-%d')
                WorldCup.matches_on(date)
              else
                WorldCup.matches
              end
-    render json: output.as_json
+    render json: output
   end
 end
