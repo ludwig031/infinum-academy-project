739bba4de0e1d5011ee8a18946bf9d26838bb133
Borna Matijanic <borna.matijanic@gmail.com>
2018-07-18 00:44:42 +0200

    Add permited params to Active admin models

--- app/admin/bookings.rb
+++ app/admin/bookings.rb
@@ -1,15 +1,6 @@
 ActiveAdmin.register Booking do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
-
+  permit_params :flight_id,
+                :user_id,
+                :no_of_seats,
+                :seat_price
 end
--- app/admin/flights.rb
+++ app/admin/flights.rb
@@ -1,15 +1,8 @@
 ActiveAdmin.register Flight do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
-
+  permit_params :company_id,
+                :name,
+                :no_of_seats,
+                :base_price,
+                :flys_at,
+                :lands_at
 end
