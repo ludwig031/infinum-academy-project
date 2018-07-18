108ee1865a7de999d4dc87b969c811b7c187aff2
Borna Matijanic <borna.matijanic@gmail.com>
2018-07-18 04:34:05 +0200

    Create booking and company factories

--- spec/factories/bookings.rb
+++ spec/factories/bookings.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :booking do
-    
+    no_of_seats 1
+    seat_price 2
+    flight { FactoryBot.create(:flight) }
+    user { FactoryBot.create(:user) }
   end
 end
--- spec/factories/companies.rb
+++ spec/factories/companies.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :company do
-    
+    name 'Ime'
   end
 end
