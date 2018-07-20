7dbaf28f3144f99e0472e4133725514de2ff6425
Borna Matijanic <borna.matijanic@gmail.com>
2018-07-20 17:16:43 +0200

    Create routes in api namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  namespace :api do
+    resources :users
+    resources :companies
+    resources :flights
+    resources :bookings
+  end
+
   ActiveAdmin.routes(self)
   get '/world-cup', to: 'application#matches'
 end
