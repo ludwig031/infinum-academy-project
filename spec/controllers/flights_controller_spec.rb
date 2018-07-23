85cf1d5a7829021f4acf629d055c1fe1827b70b3
Borna Matijanic <borna.matijanic@gmail.com>
2018-07-23 03:57:48 +0200

    Implement flights controller tests

--- spec/controllers/flights_controller_spec.rb
+++ spec/controllers/flights_controller_spec.rb
@@ -1,50 +1,123 @@
 RSpec.describe FlightsController, type: :controller do
   describe 'GET #index' do
+    let(:flights) { FactoryBot.create_list(:flight) }
+
     it 'returns http success' do
       get :index
       expect(response).to have_http_status(:success)
     end
+
+    it 'returns list of flights' do
+      get :index
+      expect(response).to be_success
+    end
   end
 
   describe 'GET #show' do
+    let(:flight) { FactoryBot.create(:flight) }
+
     it 'returns http success' do
-      get :show
+      get :show, params: { id: flight.id }
       expect(response).to have_http_status(:success)
+    end
+
+    it 'returns a single flight' do
+      get :show, params: { id: flight.id }
+      json_body = JSON.parse(response.body)
+      expect(json_body).to include('flight')
     end
   end
 
-  describe 'GET #new' do
-    it 'returns http success' do
-      get :new
-      expect(response).to have_http_status(:success)
+  describe 'POST #create' do
+    context 'when params are valid' do
+      let(:company) { FactoryBot.create(:company) }
+
+      it 'returns 201' do
+        post :create, params: { flight: { name: 'Let za doma',
+                                          flys_at: Time.zone.now + 1.hour,
+                                          lands_at: Time.zone.now + 2.hours,
+                                          base_price: 100,
+                                          no_of_seats: 200,
+                                          company_id: company.id } }
+
+        expect(response).to have_http_status(:created)
+      end
+
+      it 'creates and returns a new flight' do
+        post :create, params: { flight: { name: 'Drugi let za doma',
+                                          flys_at: Time.zone.now + 1.hour,
+                                          lands_at: Time.zone.now + 2.hours,
+                                          base_price: 100,
+                                          no_of_seats: 200,
+                                          company_id: company.id } }
+
+        json_body = JSON.parse(response.body)
+        expect(json_body).to include('flight' =>
+                                         include('name' => 'Drugi let za doma'))
+      end
+    end
+
+    context 'when params are invalid' do
+      it 'returns 400 Bad Request' do
+        post :create, params: { flight: { name: '' } }
+
+        expect(response).to have_http_status(:bad_request)
+      end
+
+      it 'returns all errors' do
+        post :create, params: { flight: { name: '' } }
+
+        json_body = JSON.parse(response.body)
+        expect(json_body).to include('errors')
+      end
     end
   end
 
-  describe 'GET #create' do
-    it 'returns http success' do
-      get :create
-      expect(response).to have_http_status(:success)
+  describe 'PATCH #update' do
+    let(:flight) { FactoryBot.create(:flight) }
+
+    context 'when params are valid' do
+      it 'returns 200 OK' do
+        post :update, params: { id: flight.id,
+                                flight: { name: 'Lufthansa' } }
+
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'returns a created booking' do
+        post :update, params: { id: flight.id,
+                                flight: { name: 'Ryanair' } }
+
+        json_body = JSON.parse(response.body)
+        expect(json_body).to include('flight' => include('name' => 'Ryanair'))
+      end
+    end
+
+    context 'when params are invalid' do
+      it 'returns 400 Bad Request' do
+        post :update, params: { id: flight.id,
+                                flight: { name: '' } }
+
+        expect(response).to have_http_status(:bad_request)
+      end
+
+      it 'returns all errors' do
+        post :update, params: { id: flight.id,
+                                flight: { name: '' } }
+
+        json_body = JSON.parse(response.body)
+        expect(json_body).to include('errors')
+      end
     end
   end
 
-  describe 'GET #edit' do
-    it 'returns http success' do
-      get :edit
-      expect(response).to have_http_status(:success)
-    end
-  end
+  describe 'DELETE #destroy' do
+    let(:flight) { FactoryBot.create(:flight) }
 
-  describe 'GET #update' do
-    it 'returns http success' do
-      get :update
-      expect(response).to have_http_status(:success)
-    end
-  end
+    it 'returns 204 No Content' do
+      post :destroy, params: { id: flight.id }
 
-  describe 'GET #destroy' do
-    it 'returns http success' do
-      get :destroy
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(:no_content)
     end
   end
 end
