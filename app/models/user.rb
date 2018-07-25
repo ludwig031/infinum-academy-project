ff40281519de9f1363cbd51c26087dd4c5f77ae0
Borna Matijanic <borna.matijanic@gmail.com>
2018-07-25 20:11:35 +0200

    Implement has secure password to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_secure_password
   has_many :bookings, dependent: :destroy
   has_many :flights, through: :bookings
   validates :first_name,
---
+++ db/migrate/20180725164100_add_password_to_users.rb
@@ -0,0 +1,11 @@
+class AddPasswordToUsers < ActiveRecord::Migration[5.2]
+  def up
+    add_column :users do
+      string 'password_digest', default: 'defaultPassword'
+    end
+  end
+
+  def down
+    remove_column :users, :password_digest
+  end
+end
