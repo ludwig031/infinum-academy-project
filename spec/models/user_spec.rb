b0ebf8404bd998f6a3f3b8f9d1d5a6ec815aadd1
Borna Matijanic <borna.matijanic@gmail.com>
2018-07-25 10:06:52 +0200

    Remove redundant validation test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,6 @@
 RSpec.describe User, type: :model do
   subject(:user) { FactoryBot.build(:user) }
 
-  it { is_expected.to be_valid }
-
   it { is_expected.to validate_presence_of(:first_name) }
 
   it { is_expected.to validate_presence_of(:last_name) }
