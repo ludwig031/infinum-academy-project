dbefaeaf9239a880aed773e528d5b96af9fa97dc
Borna Matijanic <borna.matijanic@gmail.com>
2018-07-29 01:36:18 +0200

    Remove unnecessary token validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,4 @@ class User < ApplicationRecord
   validates :password,
             presence: true,
             on: :create
-  validates :token,
-            presence: true,
-            uniqueness: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,8 +10,4 @@ RSpec.describe User, type: :model do
   it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
 
   it { is_expected.to have_secure_password }
-
-  it { is_expected.to validate_presence_of(:token) }
-
-  it { is_expected.to validate_uniqueness_of(:token) }
 end
