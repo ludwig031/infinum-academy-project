e6bb6b163ede80dd52ac6d41008f7a720769b8b0
Borna Matijanic <borna.matijanic@gmail.com>
2018-08-01 13:07:36 +0200

    Filter active flights

--- app/controllers/api/companies_controller.rb
+++ app/controllers/api/companies_controller.rb
@@ -3,7 +3,13 @@ module Api
     before_action :authentication
 
     def index
-      render json: Company.all
+      render json: if params[:filter] == 'active'
+                     Company.joins(:flights).group(:id)
+                            .where('flys_at > ?', Time.zone.now).all
+                            .order(:name)
+                   else
+                     Company.all.order(:name)
+                   end
     end
 
     def show
