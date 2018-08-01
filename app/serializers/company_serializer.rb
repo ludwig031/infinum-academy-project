74705b8aaad2627f79bcf12c23ecdfadcab647ec
Borna Matijanic <borna.matijanic@gmail.com>
2018-08-01 15:57:42 +0200

    Add no_of_active_flights to company serializer

--- app/serializers/company_serializer.rb
+++ app/serializers/company_serializer.rb
@@ -2,4 +2,9 @@ class CompanySerializer < ActiveModel::Serializer
   attribute :id
   attribute :name
   has_many :flights
+  attribute :no_of_active_flights
+
+  def no_of_active_flights
+    object.flights.where('flys_at > ?', Time.zone.now).all.size
+  end
 end
