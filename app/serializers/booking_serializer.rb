6fe32f69b7ed30b8d5e9086d115ac8ddde50c4b8
Borna Matijanic <borna.matijanic@gmail.com>
2018-08-02 00:15:43 +0200

    Add total_price to booking serializer

--- app/serializers/booking_serializer.rb
+++ app/serializers/booking_serializer.rb
@@ -2,6 +2,12 @@ class BookingSerializer < ActiveModel::Serializer
   attribute :id
   attribute :no_of_seats
   attribute :seat_price
+  attribute :total_price
   attribute :flight_id
   attribute :user_id
+  has_one :flight
+
+  def total_price
+    object.no_of_seats * object.seat_price
+  end
 end
