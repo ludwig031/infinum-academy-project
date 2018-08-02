b7ed5962a60ee4beb2c51449bd9c505bf9390e1c
Borna Matijanic <borna.matijanic@gmail.com>
2018-08-02 22:22:57 +0200

    Move company activerecord methods into scope

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,11 +1,10 @@
 class Company < ApplicationRecord
   has_many :flights, dependent: :destroy
 
+  scope :active_flights,
+        -> { joins(:flights).group(:id).where('flys_at > ?', Time.zone.now) }
+
   validates :name,
             presence: true,
             uniqueness: { case_sensitive: false }
-
-  def self.active_flights
-    joins(:flights).group(:id).where('flys_at > ?', Time.zone.now).all
-  end
 end
