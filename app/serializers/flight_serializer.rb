22316b9d5af442e3d7be0590261e881c6c6ab303
Borna Matijanic <borna.matijanic@gmail.com>
2018-08-02 03:25:31 +0200

    Add current price calculation to flight serializator

--- app/serializers/flight_serializer.rb
+++ app/serializers/flight_serializer.rb
@@ -3,6 +3,7 @@ class FlightSerializer < ActiveModel::Serializer
   attribute :name
   attribute :no_of_seats
   attribute :base_price
+  attribute :current_price
   attribute :flys_at
   attribute :lands_at
   has_one :company
@@ -16,5 +17,21 @@ class FlightSerializer < ActiveModel::Serializer
 
   def no_of_booked_seats
     bookings.sum(no_of_seats)
+  end
+
+  def days_left
+    (object.flys_at - Date.now).to_i
+  end
+
+  def price
+    (object.base_price + days_left * (object.base_price / 15)).round
+  end
+
+  def current_price
+    if price < 2 * object.base_price
+      price
+    else
+      2 * object.base_price
+    end
   end
 end
