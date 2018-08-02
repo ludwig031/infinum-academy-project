b58b916521b6a83431c73983f03b63b40d9d1458
Borna Matijanic <borna.matijanic@gmail.com>
2018-08-02 03:03:23 +0200

    Add new fields in flight serializer

--- app/serializers/flight_serializer.rb
+++ app/serializers/flight_serializer.rb
@@ -5,5 +5,16 @@ class FlightSerializer < ActiveModel::Serializer
   attribute :base_price
   attribute :flys_at
   attribute :lands_at
+  has_one :company
   attribute :company_id
+  attribute :company_name
+  has_many :bookings
+
+  def company_name
+    company&.name
+  end
+
+  def no_of_booked_seats
+    bookings.sum(no_of_seats)
+  end
 end
