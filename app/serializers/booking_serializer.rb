535a83209c22b740917fdec3b90b2777142780a7
Borna Matijanic <borna.matijanic@gmail.com>
2018-08-04 00:42:08 +0200

    Update booking serializer

--- app/serializers/booking_serializer.rb
+++ app/serializers/booking_serializer.rb
@@ -4,8 +4,23 @@ class BookingSerializer < ActiveModel::Serializer
   attribute :seat_price
   attribute :total_price
   attribute :flight_id
+  attribute :flight_name
+  attribute :flys_at
+  attribute :company_name
   attribute :user_id
   has_one :flight
+
+  def flight_name
+    object.flight.name
+  end
+
+  def flys_at
+    object.flight.flys_at
+  end
+
+  def company_name
+    object.flight.company.name
+  end
 
   def total_price
     object.no_of_seats * object.seat_price
