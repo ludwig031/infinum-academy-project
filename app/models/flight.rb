6ff7da5cebbc6679b2aa138e605bf94711b7474b
Borna Matijanic <borna.matijanic@gmail.com>
2018-08-04 20:25:25 +0200

    Update scope in flight model

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -3,8 +3,7 @@ class Flight < ApplicationRecord
   has_many :bookings, dependent: :destroy
 
   scope :by_company, lambda { |query|
-    joins(:flight)
-      .where('flys_at > ? AND company.id IN ?', Time.zone.now, query)
+    where('flys_at > ? AND company.id IN ?', Time.zone.now, query)
   }
 
   scope :active, -> { where('flys_at > ?', Time.zone.now) }
