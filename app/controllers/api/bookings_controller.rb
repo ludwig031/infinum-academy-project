bb87150c5dfdff3e36083fe335ac37d3d606fa57
Borna Matijanic <borna.matijanic@gmail.com>
2018-08-06 01:22:37 +0200

    Fix minor syntax error in booking controller

--- app/controllers/api/bookings_controller.rb
+++ app/controllers/api/bookings_controller.rb
@@ -28,9 +28,9 @@ module Api
 
     def update
       if booking
-         .update(booking_params)
+         .update(booking_params
          .merge(seat_price: seat_price(booking_params[:flight_id] ||
-                                              booking.flight_id))
+                                              booking.flight_id)))
         render json: booking
       else
         render json: { errors: booking.errors }, status: :bad_request
