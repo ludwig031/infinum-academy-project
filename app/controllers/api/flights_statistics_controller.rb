924fc6353da56774dd1b479a38aef4c18ade331a
Borna Matijanic <borna.matijanic@gmail.com>
2018-08-06 06:05:56 +0200

    Add rescue from bad request to flight statistics

--- app/controllers/api/flights_statistics_controller.rb
+++ app/controllers/api/flights_statistics_controller.rb
@@ -1,7 +1,16 @@
 module Api
   class FlightsStatisticsController < ApplicationController
+    rescue_from ActionController::BadRequest, with: :render_bad_request
+
     def index
       render json: Flight.all, each_serializer: FlightStatisticsSerializer
+    end
+
+    private
+
+    def render_bad_request
+      render json: { errors: { flight: ['is missing'] } },
+             status: :bad_request
     end
   end
 end
