0b5dc96d1d7242bb20e805146e57ba5188256568
Borna Matijanic <borna.matijanic@gmail.com>
2018-08-08 03:27:52 +0200

    Refactor companies query

--- app/controllers/api/statistics/companies_controller.rb
+++ app/controllers/api/statistics/companies_controller.rb
@@ -5,16 +5,10 @@ module Api
         render json: company_query, each_serializer: CompanyStatisticsSerializer
       end
 
+      private
+
       def company_query
-        Company.joins('LEFT JOIN flights ON flights.company_id = companies.id
-                       LEFT JOIN bookings ON bookings.flight_id = flights.id')
-               .group('companies.id')
-               .select('companies.*, companies.id AS company_id')
-               .select('coalesce(sum(bookings.no_of_seats *
-                      bookings.seat_price),0) AS total_revenue')
-               .select('coalesce(sum(bookings.no_of_seats),0)
-                      AS total_no_of_booked_seats')
-               .order('company_id')
+        FlightsQuery.new(relation: with_stats)
       end
     end
   end
