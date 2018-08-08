fd808c651e8267cdd39932a337fd3970954529f6
Borna Matijanic <borna.matijanic@gmail.com>
2018-08-08 17:38:59 +0200

    Define object for base price

--- app/calculators/flight_calculator.rb
+++ app/calculators/flight_calculator.rb
@@ -9,6 +9,6 @@ class FlightCalculator
     difference = 15 - (flight.flys_at.to_date - Date.current)
     difference = difference.negative? ? 0 : difference
     difference = difference > 15 ? 15 : difference
-    (base_price * (1 + difference / 15)).round
+    (flight.base_price * (1 + difference / 15)).round
   end
 end
