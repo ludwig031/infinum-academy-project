c27e02e9bedcfb77596f741b487e5116021f1d6a
Borna Matijanic <borna.matijanic@gmail.com>
2018-08-10 00:01:05 +0200

    Testing seat price update

--- app/forms/booking_form.rb
+++ app/forms/booking_form.rb
@@ -2,7 +2,7 @@ class BookingForm < ActiveType::Record[Booking]
   before_save :set_seat_price
 
   def set_seat_price
-    return unless no_of_seats_changed? && id.nil?
-    self.seat_price = FlightCalculator.new(flight).price if valid?
+    return unless no_of_seats_changed?
+    self.seat_price = FlightCalculator.new(flight).price
   end
 end
