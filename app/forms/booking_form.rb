61f20bcc12228b61be6535796918089df5506a46
Borna Matijanic <borna.matijanic@gmail.com>
2018-08-09 23:21:21 +0200

    Check if booking is new in validation

--- app/forms/booking_form.rb
+++ app/forms/booking_form.rb
@@ -2,7 +2,7 @@ class BookingForm < ActiveType::Record[Booking]
   before_save :set_seat_price
 
   def set_seat_price
-    return unless no_of_seats.changed?
+    return unless no_of_seats_changed? && id.exists?
     self.seat_price = FlightCalculator.new(flight).price if valid?
   end
 end
