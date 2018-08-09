6a688661aa81eb32f9c034e705e00656e3e34c6e
Borna Matijanic <borna.matijanic@gmail.com>
2018-08-09 22:53:50 +0200

    Edit ih changed condition

--- app/forms/booking_form.rb
+++ app/forms/booking_form.rb
@@ -2,7 +2,7 @@ class BookingForm < ActiveType::Record[Booking]
   before_save :set_seat_price
 
   def set_seat_price
-    return unless no_of_seats_changed?
+    return unless no_of_seats.changed?
     self.seat_price = FlightCalculator.new(flight).price if valid?
   end
 end
