c03d5d375b61013c36e46213d69779eec7d06390
Borna Matijanic <borna.matijanic@gmail.com>
2018-08-09 19:12:34 +0200

    Rewrite booking form

--- app/forms/booking_form.rb
+++ app/forms/booking_form.rb
@@ -1,23 +1,8 @@
 class BookingForm < ActiveType::Record[Booking]
-  include ActiveModel::Model
+  before_save :set_seat_price
 
-  attr_accessor :no_of_seats, :flight
-
-  validates :no_of_seats, presence: true,
-                          numericality: { greater_than: 0 }
-  validates :flight, presence: true
-
-  def save
-    return false unless valid?
-
-    Booking.create(no_of_seats: no_of_seats, flight_id: flight.id)
-  end
-
-  def first_name
-    full_name.split(' ').first
-  end
-
-  def last_name
-    full_name.split(' ').last
+  def set_seat_price
+    return unless no_of_seats_changed?
+    self.seat_price = FlightCalculator.new(flight).price if valid?
   end
 end
