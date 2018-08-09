7c4bb8efb8fcf3ff3e023c7d38ff78a72d743c0f
Borna Matijanic <borna.matijanic@gmail.com>
2018-08-09 22:59:17 +0200

    Change render_params_missing message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,7 +35,7 @@ class ApplicationController < ActionController::Base
   end
 
   def render_params_missing
-    render json: { errors: { exception.param => 'is missing' } },
+    render json: { errors: { error.model => 'is missing' } },
            status: :bad_request
   end
 
