be683e5dffc1e0165058ec880a39fde71f7e69a5
Borna Matijanic <borna.matijanic@gmail.com>
2018-08-10 07:48:45 +0200

    Create specs for booking authentication

---
+++ spec/policies/booking_policy_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe BookingPolicy do
+  subject { described_class.new(user, booking) }
+
+  let(:user) { FactoryBot.create(:user) }
+
+  before { user }
+
+  context 'when user is logged in' do
+    let(:booking) { FactoryBot.create(:booking, user: user) }
+
+    it { is_expected.to permit_action(:show) }
+    it { is_expected.to permit_action(:update) }
+    it { is_expected.to permit_action(:destroy) }
+  end
+
+  context 'when user is not logged in' do
+    let(:booking) { FactoryBot.create(:booking) }
+
+    it { is_expected.to forbid_action(:show) }
+    it { is_expected.to forbid_action(:update) }
+    it { is_expected.to forbid_action(:destroy) }
+  end
+end
