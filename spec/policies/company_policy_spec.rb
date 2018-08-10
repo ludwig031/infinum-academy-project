27c13ff344db7137ddea5afbfc7651982901e539
Borna Matijanic <borna.matijanic@gmail.com>
2018-08-10 08:13:45 +0200

    Create specs for company authentication

---
+++ spec/policies/company_policy_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe CompanyPolicy do
+  subject { described_class.new(user, record) }
+
+  let(:user) { FactoryBot.create(:user) }
+  let(:record) { FactoryBot.create(:company) }
+
+  before do
+    user
+    record
+  end
+
+  context 'when user is not logged in' do
+    it { is_expected.to permit_action(:index) }
+    it { is_expected.to permit_action(:show) }
+    it { is_expected.to permit_action(:create) }
+    it { is_expected.to permit_action(:update) }
+    it { is_expected.to permit_action(:destroy) }
+  end
+end
